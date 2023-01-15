b0a9079fbc0efaa5b7d3de1cb1746df1ec4f8666
Josue <cuellarjosue3@gmail.com>
2023-01-14 16:05:59 -0800

    Changed Pass code

--- server.js
+++ server.js
@@ -1,38 +1,44 @@
-// /server.js
-require('dotenv').config()
+// ./server.js
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
 require('./config/database');
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const PORT = process.env.PORT || 3001
 
-const app = express()
+const app = express();
 
-app.use(express.json())// req.body
-app.use((req, res, next) => {
-    res.locals.data = {}
-    next()
-})
-app.use(logger('dev'))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(logger('dev'));
+// there's no need to mount express.urlencoded middleware
+// why is that?
+app.use(express.json());
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(require('./config/checkToken'))
-/*
-app.use('/api', routes) <====== Finish code once you got it
-*/
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/fruits', require('./routes/api/fruits'))
+// Check if token and create req.user
+app.use(require('./config/checkToken'));
 
-app.get('/api/test', (req, res) => {
-    res.json({'eureka': 'you have found it'})
-})
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+// Protect the API routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.listen(PORT, () => {
-    console.log(`I am listening on ${PORT}`)
-})
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3008;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
